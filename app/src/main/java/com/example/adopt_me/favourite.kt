73119115ebb8139d2742244f65181115b8aa8bcb
saheelsaneef <saneef1231@gmail.com>
2024-03-12 06:42:33 +0530

    fix error

--- app/src/main/java/com/example/adopt_me/favourite.kt
+++ app/src/main/java/com/example/adopt_me/favourite.kt
@@ -1,11 +1,51 @@
 package com.example.adopt_me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class favourite : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.favourite -> {
+                    val intent = Intent(this, favourite::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.feed -> {
+                    val intent = Intent (this,feed::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.categoryy -> {
+                    val intent = Intent(this, categoryy::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.profile -> {
+                    val intent = Intent(this, profile::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+            val intent = Intent(this,create::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/adopt_me/feed.kt
+++ app/src/main/java/com/example/adopt_me/feed.kt
@@ -1,11 +1,51 @@
 package com.example.adopt_me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class feed : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.feed -> {
+                    val intent = Intent (this,Home::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.categoryy -> {
+                    val intent = Intent(this, categoryy::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.favourite -> {
+                    val intent = Intent(this, favourite::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.profile -> {
+                    val intent = Intent(this, profile::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+            val intent = Intent(this,create::class.java)
+            startActivity(intent)
+
+        }
     }
 }
