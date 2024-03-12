12fff7eb15b41e25ad6a7f90b17bf69205a31b6a
saheelsaneef <saneef1231@gmail.com>
2024-03-12 15:19:20 +0530

    Set back button

--- app/src/main/java/com/example/adopt_me/create.kt
+++ app/src/main/java/com/example/adopt_me/create.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class create : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,12 @@ class create : AppCompatActivity() {
             val intent = Intent(this, feed::class.java)
             startActivity(intent)
 
+        }
+
+        val backBtn = findViewById<FloatingActionButton>(R.id.fab_crt)
+        backBtn.setOnClickListener {
+            val intent = Intent(this,feed::class.java)
+            startActivity(intent)
         }
     }
 }
